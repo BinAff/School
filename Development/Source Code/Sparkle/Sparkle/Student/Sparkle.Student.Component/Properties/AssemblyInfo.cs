5931cb02a76816624b3b5dab0a0c2e579f44a9d7
Arpan Kar <arpan.kar@binaryaffairs.com>
2015-10-30 12:13:28 +0530

    Updating assembly information

--- Development/Source Code/Sparkle/Sparkle/Student/Sparkle.Student.Component/Properties/AssemblyInfo.cs
+++ Development/Source Code/Sparkle/Sparkle/Student/Sparkle.Student.Component/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sparkle.Student.Component")]
-[assembly: AssemblyDescription("Student Component")]
+[assembly: AssemblyDescription("Sparkle Student Administration : Component")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Binary Affairs")]
 [assembly: AssemblyProduct("Sparkle")]
--- Development/Source Code/Sparkle/Sparkle/Student/Sparkle.Student.Facade/Properties/AssemblyInfo.cs
+++ Development/Source Code/Sparkle/Sparkle/Student/Sparkle.Student.Facade/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sparkle.Student.Facade")]
-[assembly: AssemblyDescription("Student Facade")]
+[assembly: AssemblyDescription("Sparkle Student Administration : Facade Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Binary Affairs")]
 [assembly: AssemblyProduct("Sparkle")]
