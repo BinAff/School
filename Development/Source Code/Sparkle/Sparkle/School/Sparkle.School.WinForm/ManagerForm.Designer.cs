d29b70332b14e67a86b9678640932e54faffaf8c
Arpan Kar <arpan.kar@binaryaffairs.com>
2015-10-30 12:14:49 +0530

    Adding Sparkle School Administration : WinForm Library

---
+++ Development/Source Code/Sparkle/Sparkle/School/Sparkle.School.WinForm/ManagerForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Sparkle.School.WinForm
+{
+    partial class ManagerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSchoolInformation = new System.Windows.Forms.Button();
+            this.btnSyllabusManagement = new System.Windows.Forms.Button();
+            this.btnCourseManagement = new System.Windows.Forms.Button();
+            this.btnFeeManagement = new System.Windows.Forms.Button();
+            this.btnAnnouncementManagement = new System.Windows.Forms.Button();
+            this.btnEventManagement = new System.Windows.Forms.Button();
+            this.btnResultManagement = new System.Windows.Forms.Button();
+            this.btnAdmitionManagement = new System.Windows.Forms.Button();
+            this.btnExaminationManagement = new System.Windows.Forms.Button();
+            this.btnLibraryManagement = new System.Windows.Forms.Button();
+            this.btnLaboratoryManagement = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSchoolInformation
+            // 
+            this.btnSchoolInformation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSchoolInformation.Location = new System.Drawing.Point(12, 12);
+            this.btnSchoolInformation.Name = "btnSchoolInformation";
+            this.btnSchoolInformation.Size = new System.Drawing.Size(94, 94);
+            this.btnSchoolInformation.TabIndex = 11;
+            this.btnSchoolInformation.Text = "School Information";
+            this.btnSchoolInformation.UseVisualStyleBackColor = true;
+            this.btnSchoolInformation.Click += new System.EventHandler(this.btnSchoolInformation_Click);
+            // 
+            // btnSyllabusManagement
+            // 
+            this.btnSyllabusManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSyllabusManagement.Location = new System.Drawing.Point(212, 12);
+            this.btnSyllabusManagement.Name = "btnSyllabusManagement";
+            this.btnSyllabusManagement.Size = new System.Drawing.Size(94, 94);
+            this.btnSyllabusManagement.TabIndex = 12;
+            this.btnSyllabusManagement.Text = "Syllabus Management";
+            this.btnSyllabusManagement.UseVisualStyleBackColor = true;
+            this.btnSyllabusManagement.Click += new System.EventHandler(this.btnSyllabusManagement_Click);
+            // 
+            // btnCourseManagement
+            // 
+            this.btnCourseManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCourseManagement.Location = new System.Drawing.Point(112, 12);
+            this.btnCourseManagement.Name = "btnCourseManagement";
+            this.btnCourseManagement.Size = new System.Drawing.Size(94, 94);
+            this.btnCourseManagement.TabIndex = 13;
+            this.btnCourseManagement.Text = "Course Management";
+            this.btnCourseManagement.UseVisualStyleBackColor = true;
+            this.btnCourseManagement.Click += new System.EventHandler(this.btnCourseManagement_Click);
+            // 
+            // btnFeeManagement
+            // 
+            this.btnFeeManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFeeManagement.Location = new System.Drawing.Point(312, 12);
+            this.btnFeeManagement.Name = "btnFeeManagement";
+            this.btnFeeManagement.Size = new System.Drawing.Size(94, 94);
+            this.btnFeeManagement.TabIndex = 14;
+            this.btnFeeManagement.Text = "Fee Management";
+            this.btnFeeManagement.UseVisualStyleBackColor = true;
+            this.btnFeeManagement.Click += new System.EventHandler(this.btnFeeManagement_Click);
+            // 
+            // btnAnnouncementManagement
+            // 
+            this.btnAnnouncementManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAnnouncementManagement.Location = new System.Drawing.Point(412, 112);
+            this.btnAnnouncementManagement.Name = "btnAnnouncementManagement";
+            this.btnAnnouncementManagement.Size = new System.Drawing.Size(94, 94);
+            this.btnAnnouncementManagement.TabIndex = 15;
+            this.btnAnnouncementManagement.Text = "Announcement Management";
+            this.btnAnnouncementManagement.UseVisualStyleBackColor = true;
+            this.btnAnnouncementManagement.Click += new System.EventHandler(this.btnAnnouncementManagement_Click);
+            // 
+            // btnEventManagement
+            // 
+            this.btnEventManagement.Enabled = false;
+            this.btnEventManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEventManagement.Location = new System.Drawing.Point(312, 112);
+            this.btnEventManagement.Name = "btnEventManagement";
+            this.btnEventManagement.Size = new System.Drawing.Size(94, 94);
+            this.btnEventManagement.TabIndex = 16;
+            this.btnEventManagement.Text = "Event Management";
+            this.btnEventManagement.UseVisualStyleBackColor = true;
+            this.btnEventManagement.Click += new System.EventHandler(this.btnEventManagement_Click);
+            // 
+            // btnResultManagement
+            // 
+            this.btnResultManagement.Enabled = false;
+            this.btnResultManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResultManagement.Location = new System.Drawing.Point(212, 112);
+            this.btnResultManagement.Name = "btnResultManagement";
+            this.btnResultManagement.Size = new System.Drawing.Size(94, 94);
+            this.btnResultManagement.TabIndex = 17;
+            this.btnResultManagement.Text = "Result Management";
+            this.btnResultManagement.UseVisualStyleBackColor = true;
+            this.btnResultManagement.Click += new System.EventHandler(this.btnResultManagement_Click);
+            // 
+            // btnAdmitionManagement
+            // 
+            this.btnAdmitionManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdmitionManagement.Location = new System.Drawing.Point(12, 112);
+            this.btnAdmitionManagement.Name = "btnAdmitionManagement";
+            this.btnAdmitionManagement.Size = new System.Drawing.Size(94, 94);
+            this.btnAdmitionManagement.TabIndex = 18;
+            this.btnAdmitionManagement.Text = "Admition Management";
+            this.btnAdmitionManagement.UseVisualStyleBackColor = true;
+            this.btnAdmitionManagement.Click += new System.EventHandler(this.btnAdmitionManagement_Click);
+            // 
+            // btnExaminationManagement
+            // 
+            this.btnExaminationManagement.Enabled = false;
+            this.btnExaminationManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExaminationManagement.Location = new System.Drawing.Point(112, 112);
+            this.btnExaminationManagement.Name = "btnExaminationManagement";
+            this.btnExaminationManagement.Size = new System.Drawing.Size(94, 94);
+            this.btnExaminationManagement.TabIndex = 19;
+            this.btnExaminationManagement.Text = "Examination Management";
+            this.btnExaminationManagement.UseVisualStyleBackColor = true;
+            this.btnExaminationManagement.Click += new System.EventHandler(this.btnExaminationManagement_Click);
+            // 
+            // btnLibraryManagement
+            // 
+            this.btnLibraryManagement.Enabled = false;
+            this.btnLibraryManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLibraryManagement.Location = new System.Drawing.Point(412, 12);
+            this.btnLibraryManagement.Name = "btnLibraryManagement";
+            this.btnLibraryManagement.Size = new System.Drawing.Size(94, 94);
+            this.btnLibraryManagement.TabIndex = 20;
+            this.btnLibraryManagement.Text = "Library Management";
+            this.btnLibraryManagement.UseVisualStyleBackColor = true;
+            this.btnLibraryManagement.Click += new System.EventHandler(this.btnLibraryManagement_Click);
+            // 
+            // btnLaboratoryManagement
+            // 
+            this.btnLaboratoryManagement.Enabled = false;
+            this.btnLaboratoryManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLaboratoryManagement.Location = new System.Drawing.Point(512, 12);
+            this.btnLaboratoryManagement.Name = "btnLaboratoryManagement";
+            this.btnLaboratoryManagement.Size = new System.Drawing.Size(94, 94);
+            this.btnLaboratoryManagement.TabIndex = 21;
+            this.btnLaboratoryManagement.Text = "Laboratory Management";
+            this.btnLaboratoryManagement.UseVisualStyleBackColor = true;
+            this.btnLaboratoryManagement.Click += new System.EventHandler(this.btnLaboratoryManagement_Click);
+            // 
+            // ManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 217);
+            this.Controls.Add(this.btnLaboratoryManagement);
+            this.Controls.Add(this.btnLibraryManagement);
+            this.Controls.Add(this.btnExaminationManagement);
+            this.Controls.Add(this.btnAdmitionManagement);
+            this.Controls.Add(this.btnResultManagement);
+            this.Controls.Add(this.btnEventManagement);
+            this.Controls.Add(this.btnAnnouncementManagement);
+            this.Controls.Add(this.btnFeeManagement);
+            this.Controls.Add(this.btnCourseManagement);
+            this.Controls.Add(this.btnSyllabusManagement);
+            this.Controls.Add(this.btnSchoolInformation);
+            this.Name = "ManagerForm";
+            this.Text = "School Manager";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSchoolInformation;
+        private System.Windows.Forms.Button btnSyllabusManagement;
+        private System.Windows.Forms.Button btnCourseManagement;
+        private System.Windows.Forms.Button btnFeeManagement;
+        private System.Windows.Forms.Button btnAnnouncementManagement;
+        private System.Windows.Forms.Button btnEventManagement;
+        private System.Windows.Forms.Button btnResultManagement;
+        private System.Windows.Forms.Button btnAdmitionManagement;
+        private System.Windows.Forms.Button btnExaminationManagement;
+        private System.Windows.Forms.Button btnLibraryManagement;
+        private System.Windows.Forms.Button btnLaboratoryManagement;
+    }
+}
+
---
+++ Development/Source Code/Sparkle/Sparkle/School/Sparkle.School.WinForm/ManagerForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Sparkle.School.WinForm
+{
+
+    public partial class ManagerForm : Sparkle.Core.Presentation.PopupMenu
+    {
+
+        public ManagerForm()
+            : base()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSchoolInformation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCourseManagement_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSyllabusManagement_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFeeManagement_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLibraryManagement_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLaboratoryManagement_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdmitionManagement_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExaminationManagement_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnResultManagement_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEventManagement_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAnnouncementManagement_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+}
