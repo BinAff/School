13fad2ce5ef42fd81344addf75a5aafcf4dba731
Arpan Kar <arpan.kar@binaryaffairs.com>
2015-10-30 12:15:25 +0530

    Adding parkle Examination Administration : WinForm Library

---
+++ Development/Source Code/Sparkle/Sparkle/School/Examination/Sparkle.School.Examination.Winform/ManagementForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Sparkle.Examination.Winform
+{
+    partial class ManagementForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQuestionSetter = new System.Windows.Forms.Button();
+            this.btnResultManagement = new System.Windows.Forms.Button();
+            this.btnExaminationCalander = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnQuestionSetter
+            // 
+            this.btnQuestionSetter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuestionSetter.Location = new System.Drawing.Point(12, 12);
+            this.btnQuestionSetter.Name = "btnQuestionSetter";
+            this.btnQuestionSetter.Size = new System.Drawing.Size(94, 94);
+            this.btnQuestionSetter.TabIndex = 14;
+            this.btnQuestionSetter.Text = "Question Setter";
+            this.btnQuestionSetter.UseVisualStyleBackColor = true;
+            // 
+            // btnResultManagement
+            // 
+            this.btnResultManagement.Enabled = false;
+            this.btnResultManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResultManagement.Location = new System.Drawing.Point(112, 12);
+            this.btnResultManagement.Name = "btnResultManagement";
+            this.btnResultManagement.Size = new System.Drawing.Size(94, 94);
+            this.btnResultManagement.TabIndex = 15;
+            this.btnResultManagement.Text = "Result Management";
+            this.btnResultManagement.UseVisualStyleBackColor = true;
+            // 
+            // btnExaminationCalander
+            // 
+            this.btnExaminationCalander.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExaminationCalander.Location = new System.Drawing.Point(212, 12);
+            this.btnExaminationCalander.Name = "btnExaminationCalander";
+            this.btnExaminationCalander.Size = new System.Drawing.Size(94, 94);
+            this.btnExaminationCalander.TabIndex = 16;
+            this.btnExaminationCalander.Text = "Examination Calander";
+            this.btnExaminationCalander.UseVisualStyleBackColor = true;
+            // 
+            // ExaminationManagementForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 262);
+            this.Controls.Add(this.btnExaminationCalander);
+            this.Controls.Add(this.btnResultManagement);
+            this.Controls.Add(this.btnQuestionSetter);
+            this.Name = "ExaminationManagementForm";
+            this.Text = "Examination Manager";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQuestionSetter;
+        private System.Windows.Forms.Button btnResultManagement;
+        private System.Windows.Forms.Button btnExaminationCalander;
+    }
+}
+
---
+++ Development/Source Code/Sparkle/Sparkle/School/Examination/Sparkle.School.Examination.Winform/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sparkle.School.Examination.Winform")]
+[assembly: AssemblyDescription("Sparkle Examination Administration : WinForm Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Binary Affairs")]
+[assembly: AssemblyProduct("Sparkle")]
+[assembly: AssemblyCopyright("Copyright © Binary Affairs 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b3191d1-b59f-424a-a839-0a2c82c76fe6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
