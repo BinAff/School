ee8042f6d1bd9bacd03b6c06fa34bfdd59e42afe
Arpan Kar <arpan.kar@binaryaffairs.com>
2015-11-07 17:30:42 +0530

    Base project for school component

---
+++ Development/Source Code/Sparkle/Sparkle/School/Sparkle.School.Component/Section/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparkle.School.Component.Section
+{
+
+    public class Data : BinAff.Core.Data
+    {
+
+    }
+
+}
