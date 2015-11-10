a5449242f17c8ab6117fadc334f901098c39fb71
SudarshanAcharya <sudarshan.acharya@binaryaffairs.com>
2015-11-10 19:10:39 +0530

    Added Caste table

---
+++ Development/Source Code/Sparkle/Sparkle/Database/Configuration/Tables/Caste.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Configuration.Caste(
+	Id   BIGINT       IDENTITY (1, 1) NOT NULL,
+    Name VARCHAR (50) NOT NULL,
+
+    [ReligionID] BIGINT NOT NULL, 
+    CONSTRAINT PK_Caste PRIMARY KEY CLUSTERED (Id ASC),
+	CONSTRAINT [FK_Caste_Religion] FOREIGN KEY ([ReligionId]) REFERENCES [Configuration].[Religion] ([Id])
+);
