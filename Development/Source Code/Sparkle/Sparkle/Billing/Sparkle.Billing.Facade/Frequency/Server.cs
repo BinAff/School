6b4c78e6b7739be50f86b091c64603936353b465
Arpan Kar <arpan.kar@binaryaffairs.com>
2015-12-17 19:09:42 +0530

    bug

--- Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.Facade/Frequency/Server.cs
+++ Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.Facade/Frequency/Server.cs
@@ -1,12 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Comp = Sparkle.Billing.Component.Frequency;
+
+using FacLib = Sparkle.Core.Facade;
 
 namespace Sparkle.Billing.Facade.Frequency
 {
-    class Server
+
+    public class Server : Sparkle.Core.Facade.Server
     {
+
+        public Server(FormDto formDto)
+            : base(formDto)
+        {
+
+        }
+
+        #region Framework
+
+        protected override FacLib.FormDto InstantiateFormDto()
+        {
+            return new FormDto
+            {
+                Dto = new Dto(),
+            };
+        }
+
+        protected override FacLib.Server InstantiateFacade()
+        {
+            return new Server(base.FormDto as FormDto);
+        }
+
+        protected override void Instantiate()
+        {
+            base.ComponentData = new Comp.Data();
+            base.ComponentServer = new Comp.Server(base.ComponentData as Comp.Data);
+        }
+
+        public override void AssignDto()
+        {
+            Dto dto = (base.FormDto as FormDto).Dto as Dto;
+            Comp.Data data = base.ComponentData as Comp.Data;
+            base.ComponentData.Id = data.Id;
+            dto.Name = data.Name;
+        }
+
+        public override void AssignData()
+        {
+            Dto dto = (base.FormDto as FormDto).Dto as Dto;
+            Comp.Data data = base.ComponentData as Comp.Data;
+            data.Id = dto.Id;
+            data.Name = dto.Name;
+        }
+
+        public override BinAff.Facade.Library.Dto Convert(BinAff.Core.Data data)
+        {
+            Comp.Data dt = data as Comp.Data;
+            return new Dto
+            {
+                Id = dt.Id,
+                Name = dt.Name,
+            };
+        }
+
+        public override BinAff.Core.Data Convert(BinAff.Facade.Library.Dto dto)
+        {
+            Dto dt = dto as Dto;
+            return new Comp.Data
+            {
+                Id = dt.Id,
+                Name = dt.Name,
+            };
+        }
+
+        #endregion
+
     }
+
 }
