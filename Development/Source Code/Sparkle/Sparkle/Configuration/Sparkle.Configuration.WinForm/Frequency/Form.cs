47fbbbda1f3f884f7d274fc68502aa33ac7247e9
JigarChhedaBinAff <jigar.chheda@binaryaffairs.com>
2015-12-28 11:38:03 +0530

    Adding Frequncy WinForm

---
+++ Development/Source Code/Sparkle/Sparkle/Configuration/Sparkle.Configuration.WinForm/Frequency/Form.cs
@@ -0,0 +1,25 @@
+﻿namespace Sparkle.Finance.WinForm.Frequency
+{
+
+    public partial class Form : Sparkle.Core.Presentation.Form
+    {
+
+        public Form()
+        {
+            InitializeComponent();
+            base.ListDisplayName = "Name";
+            base.FormName = "Frequency";
+        }
+
+        #region Framework
+
+        protected override void InstantiateFormControl()
+        {
+            base.FormControl = new FormControl();
+        }
+
+        #endregion
+
+    }
+
+}
