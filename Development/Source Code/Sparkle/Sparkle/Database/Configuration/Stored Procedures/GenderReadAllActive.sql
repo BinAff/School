fe575d9d2ca03d4d90445dddde2e286e144b4710
Arpan Kar <arpan.kar@binaryaffairs.com>
2016-01-06 16:26:32 +0530

    Adding activate/deactivate

---
+++ Development/Source Code/Sparkle/Sparkle/Database/Configuration/Stored Procedures/GenderReadAllActive.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [Configuration].[GenderReadAllActive]
+AS
+BEGIN
+	
+	SELECT Id, Name, IsActive
+	FROM Configuration.Gender  WITH (NOLOCK)
+	WHERE IsActive = 1
+   
+END
--- Development/Source Code/Sparkle/Sparkle/Database/Configuration/Tables/Gender.sql
+++ Development/Source Code/Sparkle/Sparkle/Database/Configuration/Tables/Gender.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [Configuration].[Gender] (
     [Id]   BIGINT       IDENTITY (1, 1) NOT NULL,
     [Name] VARCHAR (50) NOT NULL,
+    [IsActive] BIT CONSTRAINT [DF_Gender_IsActive] DEFAULT ((1)) NOT NULL,
     CONSTRAINT [PK_Gender] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [IX_Gender] UNIQUE NONCLUSTERED ([Name] ASC)
 );
