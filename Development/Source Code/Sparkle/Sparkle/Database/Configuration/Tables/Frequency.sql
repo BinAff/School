de5bc0db68fbe31b8b597e2bab35e2578898c83c
Arpan Kar <arpan.kar@binaryaffairs.com>
2016-01-06 16:24:04 +0530

    Default constraint name change

--- Development/Source Code/Sparkle/Sparkle/Database/Configuration/Tables/Frequency.sql
+++ Development/Source Code/Sparkle/Sparkle/Database/Configuration/Tables/Frequency.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [Configuration].[Frequency] (
     [Id]       BIGINT       IDENTITY (1, 1) NOT NULL,
     [Name]     VARCHAR (50) NOT NULL,
-    [IsActive] BIT          CONSTRAINT [DF_Freq_active] DEFAULT ((1)) NOT NULL,
+    [IsActive] BIT CONSTRAINT [DF_Frequency_IsActive] DEFAULT ((1)) NOT NULL,
     CONSTRAINT [PK_Frequency] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
