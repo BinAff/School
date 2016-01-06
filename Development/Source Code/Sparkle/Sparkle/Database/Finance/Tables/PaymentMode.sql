607a439b8617c348b8dbe8548a55eede750181e2
Arpan Kar <arpan.kar@binaryaffairs.com>
2016-01-06 16:23:36 +0530

    Default constraint name change

--- Development/Source Code/Sparkle/Sparkle/Database/Finance/Tables/PaymentMode.sql
+++ Development/Source Code/Sparkle/Sparkle/Database/Finance/Tables/PaymentMode.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [Finance].[PaymentMode] (
     [Id]       BIGINT       IDENTITY (1, 1) NOT NULL,
     [Name]     VARCHAR (50) NOT NULL,
-    [IsActive] BIT          CONSTRAINT [DF__PaymentMo__IsAct__4C0144E4] DEFAULT ((1)) NOT NULL,
+    [IsActive] BIT CONSTRAINT [DF_PaymentMode_IsActive] DEFAULT ((1)) NOT NULL,
     CONSTRAINT [PK_PaymentMode] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
