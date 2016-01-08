5a481ce7ed5729fd4c65ab1fa2f8d57449542152
Arpan Kar <arpan.kar@binaryaffairs.com>
2016-01-08 11:57:34 +0530

    Fee form design done

--- Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.WinForm/Fee/FormControl.Designer.cs
+++ Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.WinForm/Fee/FormControl.Designer.cs
@@ -28,10 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
+            this.cboFeeGroup = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboFeeDefinition = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboStandard = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cboCategory = new System.Windows.Forms.ComboBox();
+            this.tlpMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpMain
+            // 
+            this.tlpMain.ColumnCount = 4;
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpMain.Controls.Add(this.cboFeeGroup, 2, 0);
+            this.tlpMain.Controls.Add(this.label2, 0, 0);
+            this.tlpMain.Controls.Add(this.label1, 0, 1);
+            this.tlpMain.Controls.Add(this.cboFeeDefinition, 2, 1);
+            this.tlpMain.Controls.Add(this.label3, 0, 2);
+            this.tlpMain.Controls.Add(this.cboStandard, 2, 2);
+            this.tlpMain.Controls.Add(this.label4, 0, 4);
+            this.tlpMain.Controls.Add(this.txtAmount, 2, 4);
+            this.tlpMain.Controls.Add(this.label5, 0, 3);
+            this.tlpMain.Controls.Add(this.cboCategory, 2, 3);
+            this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpMain.Location = new System.Drawing.Point(0, 0);
+            this.tlpMain.MinimumSize = new System.Drawing.Size(100, 52);
+            this.tlpMain.Name = "tlpMain";
+            this.tlpMain.RowCount = 7;
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpMain.Size = new System.Drawing.Size(240, 124);
+            this.tlpMain.TabIndex = 11;
+            // 
+            // cboFeeGroup
+            // 
+            this.cboFeeGroup.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboFeeGroup.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFeeGroup.FormattingEnabled = true;
+            this.cboFeeGroup.Location = new System.Drawing.Point(123, 3);
+            this.cboFeeGroup.Name = "cboFeeGroup";
+            this.cboFeeGroup.Size = new System.Drawing.Size(94, 21);
+            this.cboFeeGroup.TabIndex = 166;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 24);
+            this.label2.TabIndex = 167;
+            this.label2.Text = "Fee Group";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 24);
+            this.label1.TabIndex = 168;
+            this.label1.Text = "Fee Definition";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // cboFeeDefinition
+            // 
+            this.cboFeeDefinition.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboFeeDefinition.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFeeDefinition.FormattingEnabled = true;
+            this.cboFeeDefinition.Location = new System.Drawing.Point(123, 27);
+            this.cboFeeDefinition.Name = "cboFeeDefinition";
+            this.cboFeeDefinition.Size = new System.Drawing.Size(94, 21);
+            this.cboFeeDefinition.TabIndex = 169;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(3, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 24);
+            this.label3.TabIndex = 170;
+            this.label3.Text = "Standard";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // cboStandard
+            // 
+            this.cboStandard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cboStandard.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboStandard.FormattingEnabled = true;
+            this.cboStandard.Location = new System.Drawing.Point(123, 51);
+            this.cboStandard.Name = "cboStandard";
+            this.cboStandard.Size = new System.Drawing.Size(94, 21);
+            this.cboStandard.TabIndex = 171;
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(3, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 24);
+            this.label4.TabIndex = 172;
+            this.label4.Text = "Amount";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtAmount.Location = new System.Drawing.Point(123, 99);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(94, 20);
+            this.txtAmount.TabIndex = 173;
+            // 
+            // label5
+            // 
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(3, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 24);
+            this.label5.TabIndex = 174;
+            this.label5.Text = "Category";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // cboCategory
+            // 
+            this.cboCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboCategory.FormattingEnabled = true;
+            this.cboCategory.Location = new System.Drawing.Point(123, 75);
+            this.cboCategory.Name = "cboCategory";
+            this.cboCategory.Size = new System.Drawing.Size(94, 21);
+            this.cboCategory.TabIndex = 175;
+            // 
+            // FormControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tlpMain);
+            this.MinimumSize = new System.Drawing.Size(240, 124);
+            this.Name = "FormControl";
+            this.Size = new System.Drawing.Size(240, 124);
+            this.tlpMain.ResumeLayout(false);
+            this.tlpMain.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpMain;
+        private System.Windows.Forms.ComboBox cboFeeGroup;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboFeeDefinition;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboStandard;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cboCategory;
     }
 }
