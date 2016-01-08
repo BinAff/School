2ecbc15ecfdf4f7df12f152897bb0a8eb130437a
Arpan Kar <arpan.kar@binaryaffairs.com>
2016-01-08 14:15:49 +0530

    Adding empty Bill form and form control

---
+++ Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.WinForm/Form.cs
@@ -0,0 +1,25 @@
+﻿namespace Sparkle.Billing.WinForm
+{
+
+    public partial class Form : Sparkle.Core.Presentation.Form
+    {
+
+        public Form()
+        {
+            InitializeComponent();
+            base.ListDisplayName = "Name";
+            base.FormName = "Bill";
+        }
+
+        #region Framework
+
+        protected override void InstantiateFormControl()
+        {
+            base.FormControl = new FormControl();
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.WinForm/FormControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Fac = Sparkle.Billing.Facade;
+
+namespace Sparkle.Billing.WinForm
+{
+    public partial class FormControl : Sparkle.Core.Presentation.FormControl
+    {
+        public FormControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
