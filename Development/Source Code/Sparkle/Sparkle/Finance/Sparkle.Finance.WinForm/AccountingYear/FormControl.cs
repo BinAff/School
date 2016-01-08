8661547fed87550077776552696d8803f22287ba
Arpan Kar <arpan.kar@binaryaffairs.com>
2016-01-08 11:46:02 +0530

    changing control name

--- Development/Source Code/Sparkle/Sparkle/Finance/Sparkle.Finance.WinForm/AccountingYear/FormControl.cs
+++ Development/Source Code/Sparkle/Sparkle/Finance/Sparkle.Finance.WinForm/AccountingYear/FormControl.cs
@@ -26,7 +26,7 @@ namespace Sparkle.Finance.WinForm.AccountingYear
         protected override void ClearForm()
         {
             this.txtName.Text = String.Empty;
-            this.rdlCurrYrYes.Select();
+            this.rdlYes.Select();
             this.txtName.Focus();
         }
 
@@ -34,14 +34,14 @@ namespace Sparkle.Finance.WinForm.AccountingYear
         {
             Fac.Dto dto = base.FormDto.Dto as Fac.Dto;
             dto.Name = this.txtName.Text;
-            dto.CurrentFlag = (rdlCurrYrYes.Checked == true) ? true : false;
+            dto.CurrentFlag = (rdlYes.Checked == true) ? true : false;
         }
 
         protected override void AssignFormControls()
         {
             Fac.Dto dto = base.FormDto.Dto as Fac.Dto;
             this.txtName.Text = dto.Name;
-            this.rdlCurrYrYes.Checked = (dto.CurrentFlag == true) ? true : false;
+            this.rdlYes.Checked = (dto.CurrentFlag == true) ? true : false;
         }
 
         protected override Boolean ValidateForm()
