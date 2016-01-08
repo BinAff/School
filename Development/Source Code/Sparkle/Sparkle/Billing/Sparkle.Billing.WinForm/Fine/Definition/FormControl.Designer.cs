9d39e9761a7727900e830627d8ac64b29d1cddca
Arpan Kar <arpan.kar@binaryaffairs.com>
2016-01-08 11:41:20 +0530

    Adding Fine def form control

--- Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.WinForm/Fine/Definition/FormControl.Designer.cs
+++ Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.WinForm/Fine/Definition/FormControl.Designer.cs
@@ -28,10 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rdlCurrYrNo = new System.Windows.Forms.RadioButton();
+            this.rdlCurrYrYes = new System.Windows.Forms.RadioButton();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpMain.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpMain
+            // 
+            this.tlpMain.ColumnCount = 4;
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpMain.Controls.Add(this.label1, 0, 0);
+            this.tlpMain.Controls.Add(this.txtName, 2, 0);
+            this.tlpMain.Controls.Add(this.label2, 0, 1);
+            this.tlpMain.Controls.Add(this.tableLayoutPanel2, 2, 1);
+            this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpMain.Location = new System.Drawing.Point(0, 0);
+            this.tlpMain.MinimumSize = new System.Drawing.Size(100, 28);
+            this.tlpMain.Name = "tlpMain";
+            this.tlpMain.RowCount = 3;
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpMain.Size = new System.Drawing.Size(380, 52);
+            this.tlpMain.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 24);
+            this.label1.TabIndex = 162;
+            this.label1.Text = "Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtName
+            // 
+            this.txtName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtName.Location = new System.Drawing.Point(193, 3);
+            this.txtName.MaxLength = 50;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(164, 20);
+            this.txtName.TabIndex = 165;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 24);
+            this.label2.TabIndex = 166;
+            this.label2.Text = "Is the fine taken as percentage?";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // rdlCurrYrNo
+            // 
+            this.rdlCurrYrNo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rdlCurrYrNo.Location = new System.Drawing.Point(88, 3);
+            this.rdlCurrYrNo.Name = "rdlCurrYrNo";
+            this.rdlCurrYrNo.Size = new System.Drawing.Size(79, 18);
+            this.rdlCurrYrNo.TabIndex = 168;
+            this.rdlCurrYrNo.TabStop = true;
+            this.rdlCurrYrNo.Text = "No";
+            this.rdlCurrYrNo.UseVisualStyleBackColor = true;
+            // 
+            // rdlCurrYrYes
+            // 
+            this.rdlCurrYrYes.Checked = true;
+            this.rdlCurrYrYes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rdlCurrYrYes.Location = new System.Drawing.Point(3, 3);
+            this.rdlCurrYrYes.Name = "rdlCurrYrYes";
+            this.rdlCurrYrYes.Size = new System.Drawing.Size(79, 18);
+            this.rdlCurrYrYes.TabIndex = 167;
+            this.rdlCurrYrYes.TabStop = true;
+            this.rdlCurrYrYes.Text = "Yes";
+            this.rdlCurrYrYes.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.rdlCurrYrNo, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.rdlCurrYrYes, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(190, 24);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(170, 24);
+            this.tableLayoutPanel2.TabIndex = 170;
+            // 
+            // FormControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tlpMain);
+            this.MinimumSize = new System.Drawing.Size(380, 52);
+            this.Name = "FormControl";
+            this.Size = new System.Drawing.Size(380, 52);
+            this.tlpMain.ResumeLayout(false);
+            this.tlpMain.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpMain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rdlCurrYrYes;
+        private System.Windows.Forms.RadioButton rdlCurrYrNo;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
     }
 }
