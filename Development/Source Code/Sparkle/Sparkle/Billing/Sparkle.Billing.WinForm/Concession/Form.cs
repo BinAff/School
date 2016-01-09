b312c3737f79bf351c6bab4a9a5f2eb72898dffe
Arpan Kar <arpan.kar@binaryaffairs.com>
2016-01-09 13:36:55 +0530

    Adding concession Form and FormControl draft

---
+++ Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.WinForm/Concession/Form.cs
@@ -0,0 +1,16 @@
+﻿namespace Sparkle.Billing.WinForm.Concession
+{
+
+    public partial class Form : Sparkle.Core.Presentation.Form
+    {
+
+        public Form()
+        {
+            InitializeComponent();
+            base.ListDisplayName = "Name";
+            base.FormName = "Concession";
+        }
+
+    }
+
+}
---
+++ Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.WinForm/Concession/FormControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Fac = Sparkle.Billing.Facade.Concession;
+
+namespace Sparkle.Billing.WinForm.Concession
+{
+
+    public partial class FormControl : Sparkle.Core.Presentation.FormControl
+    {
+
+        public FormControl()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
