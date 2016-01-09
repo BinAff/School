2b902ee1526a5eabfd0858f08dc84ead400c6a2d
Arpan Kar <arpan.kar@binaryaffairs.com>
2016-01-09 21:54:46 +0530

    Adding student category

---
+++ Development/Source Code/Sparkle/Sparkle/Student/Sparkle.Student.WinForm/Category/Form.cs
@@ -0,0 +1,25 @@
+﻿namespace Sparkle.Student.WinForm.Category
+{
+
+    public partial class Form : Sparkle.Core.Presentation.Form
+    {
+
+        public Form()
+        {
+            InitializeComponent();
+            base.ListDisplayName = "Name";
+            base.FormName = "Student Category";
+        }
+
+        #region Framework
+
+        protected override void InstantiateFormControl()
+        {
+            base.FormControl = new FormControl();
+        }
+
+        #endregion
+
+    }
+
+}
