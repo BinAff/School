612c67c0abf08e6c847205c85d9bbbec0ff20673
JigarChhedaBinAff <jigar.chheda@binaryaffairs.com>
2016-01-11 20:22:45 +0530

    Data Matched with dto

--- Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.Component/Concession/Data.cs
+++ Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.Component/Concession/Data.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-
+using Comp = Crystal.Configuration.Component.Caste;
 namespace Sparkle.Billing.Component.Concession
 {
 
     public class Data : BinAff.Core.Data
     {
+        public Fee.Definition.Data FeeDefinition { get; set; }
+
+        public Comp.Data Caste { get; set; }
 
         public Double Amount { get; set; }
 
         public Boolean IsPercentage { get; set; }
 
-
+        public Data()
+        {
+            FeeDefinition = new Fee.Definition.Data();
+            Caste = new Comp.Data();
+        }
     }
 
 }
