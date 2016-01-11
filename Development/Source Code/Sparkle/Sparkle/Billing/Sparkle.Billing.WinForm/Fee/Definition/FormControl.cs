400a2ba7992e7d668d169f3239790a1435a67e26
JigarChhedaBinAff <jigar.chheda@binaryaffairs.com>
2016-01-11 12:02:50 +0530

    Fee Defination UI updated

--- Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.WinForm/Fee/Definition/FormControl.cs
+++ Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.WinForm/Fee/Definition/FormControl.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 
+using BinAff.Presentation.Library.Extension;
+
+using Bil = Sparkle.Billing.Facade;
+
 using Fac = Sparkle.Billing.Facade.Fee.Definition;
 
 namespace Sparkle.Billing.WinForm.Fee.Definition
@@ -20,25 +24,35 @@ namespace Sparkle.Billing.WinForm.Fee.Definition
 
         protected override void Bind()
         {
-
+            this.cboFeeGroup.Bind((this.FormDto as Fac.FormDto).FeeGroupList, "Name");
+            this.cboFineDe.Bind((this.FormDto as Fac.FormDto).FineDefinationList, "Name");
         }
 
         protected override void ClearForm()
         {
-            //this.txtName.Text = String.Empty;
-            //this.txtName.Focus();
+            this.txtName.Text = String.Empty;
+            this.cboFeeGroup.SelectedIndex = -1;
+            this.cboFeeGroup.Text = String.Empty;
+            this.cboFineDe.SelectedIndex = -1;
+            this.cboFineDe.Text = String.Empty;
+            this.txtName.Focus();
         }
 
         protected override void AssignDto()
         {
             Fac.Dto dto = base.FormDto.Dto as Fac.Dto;
-            //dto.Name = this.txtName.Text;
+            dto.Name = this.txtName.Text;
+            dto.FeeGroup = this.cboFeeGroup.SelectedItem as Bil.Fee.Group.Dto;
+            dto.FineDefination = this.cboFineDe.SelectedItem as Bil.Fine.Definition.Dto;
+
         }
 
         protected override void AssignFormControls()
         {
             Fac.Dto dto = base.FormDto.Dto as Fac.Dto;
-            //this.txtName.Text = dto.Name;
+            this.txtName.Text = dto.Name;
+            this.cboFeeGroup.SelectedItem = (this.FormDto as Fac.FormDto).FeeGroupList.FindLast((p) => { return p.Id == dto.FeeGroup.Id; }); ;
+            this.cboFineDe.SelectedItem = (this.FormDto as Fac.FormDto).FineDefinationList.FindLast((p) => { return p.Id == dto.FineDefination.Id; }); ;
         }
 
         protected override Boolean ValidateForm()
