24d12cf6b424134732b85a958253e2229db24e08
JigarChhedaBinAff <jigar.chheda@binaryaffairs.com>
2016-01-11 12:01:35 +0530

    Fee UI updated

--- Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.WinForm/Fee/FormControl.cs
+++ Development/Source Code/Sparkle/Sparkle/Billing/Sparkle.Billing.WinForm/Fee/FormControl.cs
@@ -1,5 +1,13 @@
 ﻿using System;
 
+using BinAff.Presentation.Library.Extension;
+
+using Bil = Sparkle.Billing.Facade;
+
+using Sch = Sparkle.School.Facade;
+
+using Stu = Sparkle.Student.Facade;
+
 using Fac = Sparkle.Billing.Facade.Fee;
 
 namespace Sparkle.Billing.WinForm.Fee
@@ -20,25 +28,44 @@ namespace Sparkle.Billing.WinForm.Fee
 
         protected override void Bind()
         {
-
+            this.cboFeeGroup.Bind((this.FormDto as Fac.FormDto).FeeGroupList, "Name");
+            this.cboFeeDefinition.Bind((this.FormDto as Fac.FormDto).FeeDefinationList, "Name");
+            this.cboStandard.Bind((this.FormDto as Fac.FormDto).SchoolStandardList, "Name");
+            this.cboCategory.Bind((this.FormDto as Fac.FormDto).CategoryList, "Name");
         }
 
         protected override void ClearForm()
         {
-            //this.txtName.Text = String.Empty;
-            //this.txtName.Focus();
+            this.cboFeeGroup.Text = String.Empty;
+            this.cboFeeGroup.SelectedIndex = -1;
+            this.cboFeeDefinition.Text = String.Empty;
+            this.cboFeeDefinition.SelectedIndex = -1;
+            this.cboStandard.Text = String.Empty;
+            this.cboStandard.SelectedIndex = -1;
+            this.cboCategory.Text = String.Empty;
+            this.cboCategory.SelectedIndex = -1;
+            this.txtAmount.Text = String.Empty;
+            this.cboFeeGroup.Focus();
         }
 
         protected override void AssignDto()
         {
             Fac.Dto dto = base.FormDto.Dto as Fac.Dto;
-            //dto.Name = this.txtName.Text;
+            dto.FeeGroup = this.cboFeeGroup.SelectedItem as Bil.Fee.Group.Dto;
+            dto.FeeDefination = this.cboFeeDefinition.SelectedItem as Bil.Fee.Definition.Dto;
+            dto.SchoolStandard = this.cboStandard.SelectedItem as Sch.Standard.Dto;
+            dto.Category = this.cboCategory.SelectedItem as Stu.Category.Dto;
+            dto.Amount = Convert.ToDouble(this.txtAmount.Text);
         }
 
         protected override void AssignFormControls()
         {
             Fac.Dto dto = base.FormDto.Dto as Fac.Dto;
-            //this.txtName.Text = dto.Name;
+            this.cboFeeGroup.SelectedItem = (this.FormDto as Fac.FormDto).FeeGroupList.FindLast((p) => { return p.Id == dto.FeeGroup.Id; });
+            this.cboFeeDefinition.SelectedItem = (this.FormDto as Fac.FormDto).FeeDefinationList.FindLast((p) => { return p.Id == dto.FeeDefination.Id; });
+            this.cboStandard.SelectedItem = (this.FormDto as Fac.FormDto).SchoolStandardList.FindLast((p) => { return p.Id == dto.SchoolStandard.Id; });
+            this.cboCategory.SelectedItem = (this.FormDto as Fac.FormDto).CategoryList.FindLast((p) => { return p.Id == dto.Category.Id; });
+            this.txtAmount.Text = Convert.ToString(dto.Amount);
         }
 
         protected override Boolean ValidateForm()
