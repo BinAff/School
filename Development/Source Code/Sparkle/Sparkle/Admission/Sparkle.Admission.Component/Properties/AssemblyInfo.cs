be1f5d967cf711e090803309ccfb32bdd5879bf9
Arpan Kar <arpan.kar@binaryaffairs.com>
2016-01-12 16:11:30 +0530

    ???

--- Development/Source Code/Sparkle/Sparkle/Admission/Sparkle.Admission.Component/Properties/AssemblyInfo.cs
+++ Development/Source Code/Sparkle/Sparkle/Admission/Sparkle.Admission.Component/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sparkle.Admission.Component")]
-[assembly: AssemblyDescription("Sparkle Admission : Component")]
+[assembly: AssemblyDescription("Sparkle Admission Administration : Component Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Binary Affairs")]
 [assembly: AssemblyProduct("Sparkle")]
--- Development/Source Code/Sparkle/Sparkle/Admission/Sparkle.Admission.WinForm/Properties/AssemblyInfo.cs
+++ Development/Source Code/Sparkle/Sparkle/Admission/Sparkle.Admission.WinForm/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sparkle.Admission.WinForm")]
-[assembly: AssemblyDescription("Sparkle Admission : WinForm")]
+[assembly: AssemblyDescription("Sparkle Admission Administration : WinForm Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Binary Affairs")]
 [assembly: AssemblyProduct("Sparkle")]
